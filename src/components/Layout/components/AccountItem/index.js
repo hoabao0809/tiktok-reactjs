f8ee207899916bdba9e2e3aebd8c1a6f5d2bc08a
hoabao0809 <hoabao.0809@gmail.com>
2022-05-29 00:25:39 +0700

    New: tippyjs lib popper search box

---
+++ src/components/Layout/components/AccountItem/index.js
@@ -0,0 +1,27 @@
+import styles from '../AccountItem/AccountItem.module.scss';
+import classNames from 'classnames/bind';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCheckCircle } from '@fortawesome/free-solid-svg-icons';
+
+const cx = classNames.bind(styles);
+
+function AccountItem() {
+  return (
+    <div className={cx('wrapper')}>
+      <img
+        className={cx('avatar')}
+        src="https://p16-sign-va.tiktokcdn.com/tos-useast2a-avt-0068-giso/5823bd47b11dd97982c4fbe011408f9c~c5_300x300.webp?x-expires=1653926400&x-signature=rIvNyylIV0IFAiMdn5XWmRaO4b4%3D"
+        alt=""
+      />
+      <div className={cx('info')}>
+        <h4 className={cx('name')}>
+          <span>Nguyen Van A</span>
+          <FontAwesomeIcon className={cx('check')} icon={faCheckCircle} />
+        </h4>
+        <span className={cx('username')}>nguyenvana</span>
+      </div>
+    </div>
+  );
+}
+
+export default AccountItem;
